43936961fa75fe74a7c8ac871bc8caf18a6f0329
ealpha072 <ealpha072@gmail.com>
2022-01-26 21:54:29 +0300

    Initial file upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let commonfields = ['Author', 'Corporate Author', 'Title', 'Year'], sourcetypes = ['Book', 'Book Section','Journal Article', 'Article in a periodical', 'Report', 'Website', 'Document from Website' ],
+citation_options = ['Harvard', 'APA', 'MLA', 'Chicago']
+
