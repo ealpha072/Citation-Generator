4c63b61ab5e6b9e025bdc2fe4ec0d407343341d6
ealpha072 <ealpha072@gmail.com>
2022-01-27 22:29:55 +0300

    Select option population

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let commonfields = ['Author', 'Corporate Author', 'Title', 'Year'], sourcetypes = ['Book', 'Book Section','Journal Article', 'Article in a periodical', 'Report', 'Website', 'Document from Website' ],
 citation_options = ['Harvard', 'APA', 'MLA', 'Chicago']
 
+//dom elements
+let styles_select = $('#style-select'), bibliography_select = $('#bib-select'), type_select = $('#type-select')
+
+$(document).ready(function(){
+    citation_options.forEach(element => {
+        option = `<option>${element}</option>`
+        styles_select.append(option)
+    });
+    sourcetypes.forEach(type=>{
+        option = `<option>${type}</option>`
+        type_select.append(option)
+    })
+})
