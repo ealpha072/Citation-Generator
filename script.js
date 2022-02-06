8d2c894957353de8036cd215fc1385649f92f92e
ealpha072 <ealpha072@gmail.com>
2022-02-06 21:11:29 +0300

    Further book reference for APA

--- script.js
+++ script.js
@@ -95,45 +95,65 @@ const generateForm = (commonFields) => {
 }
 
 class APA {
-    constructor(authors, title, year){
+    constructor(authors, title, year, publisher){
         this.authors = authors
         this.title = title
         this.year = year
+        this.publisher = publisher
     }
 
-    bookReference(commonFields, bookfields) {
-        let reference = ''
+    
 
+    bookReference() {
+        //below is for non organizational authors
         function sanitizeAuthor(author){
             //takes in array of authors
             //['Kelly Rob']
             let numAuthors = author.length
-
-            switch (numAuthors) {
-                case 1:
-                    var authorNameArray = Array.from(author[0].split(' '))
-                    let lastName = authorNameArray[authorNameArray.length - 1], 
+            
+            if(numAuthors === 1){
+                var authorNameArray = Array.from(author[0].split(' '))
+                let lastName = authorNameArray[authorNameArray.length - 1], 
+                    firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                    middleInitial = authorNameArray[1].charAt(0).toUpperCase()
+                return `${lastName}, ${firstInitial}.${middleInitial}`
+            }else if(numAuthors === 2){
+                let twoAuthors = [];
+                author.forEach((item) => {
+                    var authorNameArray = Array.from(item.split(" "));
+                    let lastName = authorNameArray[authorNameArray.length - 1],
                         firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                        middleInitial = authorNameArray[1].charAt(0).toUpperCase()
-                    return `${lastName}, ${firstInitial}.${middleInitial}`
-
-                case 2:
-                    let twoAuthors = []
-                    author.forEach(item=>{
-                        var authorNameArray = Array.from(item[0].split(' '))
-                        let lastName = authorNameArray[authorNameArray.length - 1], 
-                            firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                            middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
-                            authorFullRef= `${lastName}, ${firstInitial}.${middleInitial}`
-                        twoAuthors.push(authorFullRef)
-                    })
-                    return twoAuthors.join(' & ');
-                
-                case numAuthors == 3:
-
-                default:
-                    break;
+                        middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
+                        authorFullRef = `${lastName}, ${firstInitial}.${middleInitial}.`;
+                    twoAuthors.push(authorFullRef);
+                })
+                return twoAuthors.join(" & ")
+            }else if(numAuthors > 2 && numAuthors <=20){
+                let manyAuthors = []
+                author.forEach(item=>{
+                    var authorNameArray = Array.from(item.split(" "));
+                    //console.log(authorNameArray)
+                    let lastName = authorNameArray[authorNameArray.length - 1],
+                        firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                        middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
+                        authorFullRef = `${lastName}, ${firstInitial}. ${middleInitial}`;
+                    manyAuthors.push(authorFullRef);
+                })
+               let finalList = '', finalName = manyAuthors[manyAuthors.length -1]
+        
+                for (let i = 0;  i < manyAuthors.length - 1; i ++) {
+                    finalList +=  manyAuthors[i] + ', '
+                }
+                finalList += ` & ${finalName}`
+                return finalList
             }
         }
+        //code for organization aurthor
+        let authorRef = sanitizeAuthor(this.authors)
+        var reference  = `${authorRef}.(${this.year}).${this.title}.${this.publisher} `
+        //code for organization aurthor
+        return reference
     }
+
+    journalReference(){}
 }
--- tests.js
+++ tests.js
@@ -1,28 +1,63 @@
-function sanitizeAuthor(author){
-    //takes in array of authors
-    //['Kelly Rob']
-    let numAuthors = author.length
-    console.log(numAuthors)
-    switch (numAuthors) {
-        case 1:
-            var authorNameArray = Array.from(author[0].split(' '))
-            console.log(authorNameArray)
-            let lastName = authorNameArray[authorNameArray.length - 1], 
-                firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                middleInitial = authorNameArray[1].charAt(0).toUpperCase()
-            console.log(`${lastName}, ${firstInitial}. ${middleInitial}.`)
-        case 2:
-            let authors = []
-            author.forEach(item=>{
-                var authorNameArray = Array.from(item.split(' '))
+class APA {
+    constructor(authors, title, year, publisher){
+        this.authors = authors
+        this.title = title
+        this.year = year
+        this.publisher = publisher
+    }
+
+    bookReference() {
+        //below is for non organizational authors
+        function sanitizeAuthor(author){
+            //takes in array of authors
+            //['Kelly Rob']
+            let numAuthors = author.length
+            
+            if(numAuthors === 1){
+                var authorNameArray = Array.from(author[0].split(' '))
                 let lastName = authorNameArray[authorNameArray.length - 1], 
                     firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                    middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
-                    authorFullRef= `${lastName}, ${firstInitial}. ${middleInitial}.`
-                authors.push(authorFullRef)
-            })
-            console.log(authors.join(' & '))
+                    middleInitial = authorNameArray[1].charAt(0).toUpperCase()
+                return `${lastName}, ${firstInitial}.${middleInitial}`
+            }else if(numAuthors === 2){
+                let twoAuthors = [];
+                author.forEach((item) => {
+                    var authorNameArray = Array.from(item.split(" "));
+                    let lastName = authorNameArray[authorNameArray.length - 1],
+                        firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                        middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
+                        authorFullRef = `${lastName}, ${firstInitial}.${middleInitial}.`;
+                    twoAuthors.push(authorFullRef);
+                })
+                return twoAuthors.join(" & ")
+            }else if(numAuthors > 2 && numAuthors <=20){
+                let manyAuthors = []
+                author.forEach(item=>{
+                    var authorNameArray = Array.from(item.split(" "));
+                    //console.log(authorNameArray)
+                    let lastName = authorNameArray[authorNameArray.length - 1],
+                        firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                        middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
+                        authorFullRef = `${lastName}, ${firstInitial}. ${middleInitial}`;
+                    manyAuthors.push(authorFullRef);
+                })
+               let finalList = '', finalName = manyAuthors[manyAuthors.length -1]
+        
+                for (let i = 0;  i < manyAuthors.length - 1; i ++) {
+                    finalList +=  manyAuthors[i] + ', '
+                }
+                finalList += ` & ${finalName}`
+                return finalList
+            }
+        }
+        //code for organization aurthor
+        let authorRef = sanitizeAuthor(this.authors)
+        var reference  = `${authorRef}.(${this.year}).${this.title}.${this.publisher} `
+        //code for organization aurthor
+        return reference
     }
+
+    journalReference()
 }
 
-sanitizeAuthor(['Alpha Emmanuel Ochieng', 'David Omollo'])
+const myApa = new APA(['Alpha Emmanuel Ochieng', 'Brenda Alex Wright', 'Alex Morgan Charity'], 'Becoming Obama', 2009, 'Oxford Press')
