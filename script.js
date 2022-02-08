9331f133a3c1d788dcae220ccdf970b4c48ae0a7
ealpha072 <ealpha072@gmail.com>
2022-02-08 22:02:40 +0300

    Changed class declaration for reference

--- script.js
+++ script.js
@@ -94,69 +94,6 @@ const generateForm = (commonFields) => {
     })
 }
 
-/*class APA {
-    constructor(authors, title, year, publisher){
-        this.authors = authors
-        this.title = title
-        this.year = year
-        this.publisher = publisher
-    }
-
-
-
-    bookReference() {
-        //below is for non organizational authors
-        function sanitizeAuthor(author){
-            //takes in array of authors
-            //['Kelly Rob']
-            let numAuthors = author.length
-            
-            if(numAuthors === 1){
-                var authorNameArray = Array.from(author[0].split(' '))
-                let lastName = authorNameArray[authorNameArray.length - 1], 
-                    firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                    middleInitial = authorNameArray[1].charAt(0).toUpperCase()
-                return `${lastName}, ${firstInitial}.${middleInitial}`
-            }else if(numAuthors === 2){
-                let twoAuthors = [];
-                author.forEach((item) => {
-                    var authorNameArray = Array.from(item.split(" "));
-                    let lastName = authorNameArray[authorNameArray.length - 1],
-                        firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                        middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
-                        authorFullRef = `${lastName}, ${firstInitial}.${middleInitial}.`;
-                    twoAuthors.push(authorFullRef);
-                })
-                return twoAuthors.join(" & ")
-            }else if(numAuthors > 2 && numAuthors <=20){
-                let manyAuthors = []
-                author.forEach(item=>{
-                    var authorNameArray = Array.from(item.split(" "));
-                    //console.log(authorNameArray)
-                    let lastName = authorNameArray[authorNameArray.length - 1],
-                        firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
-                        middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
-                        authorFullRef = `${lastName}, ${firstInitial}. ${middleInitial}`;
-                    manyAuthors.push(authorFullRef);
-                })
-               let finalList = '', finalName = manyAuthors[manyAuthors.length -1]
-        
-                for (let i = 0;  i < manyAuthors.length - 1; i ++) {
-                    finalList +=  manyAuthors[i] + ', '
-                }
-                finalList += ` & ${finalName}`
-                return finalList
-            }
-        }
-        //code for organization aurthor
-        let authorRef = sanitizeAuthor(this.authors)
-        var reference  = `${authorRef}.(${this.year}).${this.title}.${this.publisher} `
-        //code for organization aurthor
-        return reference
-    }
-
-    journalReference(){}
-}*/
 
 function APA(authors, title, year, publisher){
     this.authors = authors
@@ -214,7 +151,7 @@ function APA(authors, title, year, publisher){
         return reference
     }
 
-    this.reportReference = (/*takes in report number as argument */reportNum, location) => {
+    this.reportReference = (reportNum, location) => {
         let author = this.sanitizeAuthor(this.author)
         //Author, Initial. (Year). Title of report (Report No. xxx). Location: Publisher.
         let reference = `${author}. (${this.year}). ${this.title} (${reportNum}). ${location}: ${this.publisher}`
@@ -315,3 +252,70 @@ function APA(authors, title, year, publisher){
        return reference
     }
 }
+
+function Reference(style){
+    this.style = style
+    /*this.source = source
+    this.object = object*/
+    this.sanitizeAuthor = (author)=>{
+        let numAuthors = author.length
+        
+        if(numAuthors === 1){
+            var authorNameArray = Array.from(author[0].split(' '))
+            let lastName = authorNameArray[authorNameArray.length - 1], 
+                firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                middleInitial = authorNameArray[1].charAt(0).toUpperCase()
+                //check the end of initials punctuation
+            switch(this.style){
+                case 'APA':
+                    return `${lastName}, ${firstInitial}.${middleInitial}`
+                case 'Harvard':
+                    return `${lastName}, ${firstInitial}${middleInitial},`
+            }
+        }else if(numAuthors === 2){
+            let twoAuthors = [];
+            author.forEach((item) => {
+                var authorNameArray = Array.from(item.split(" "));
+                let lastName = authorNameArray[authorNameArray.length - 1],
+                    firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                    middleInitial = authorNameArray[1].charAt(0).toUpperCase();
+                    authorFullRef = ''
+                switch(this.style){
+                    case 'APA':
+                        authorFullRef = `${lastName}, ${firstInitial}.${middleInitial}.,`;
+                        twoAuthors.push(authorFullRef)
+                    case 'Harvard':
+                        authorFullRef = `${lastName}, ${firstInitial}${middleInitial},`
+                        twoAuthors.push(authorFullRef)
+                }
+            })
+            return twoAuthors.join(" & ")
+        }else if(numAuthors > 2 && numAuthors <=20){
+            let manyAuthors = []
+            author.forEach(item=>{
+                var authorNameArray = Array.from(item.split(" "));
+                //console.log(authorNameArray)
+                let lastName = authorNameArray[authorNameArray.length - 1],
+                    firstInitial = authorNameArray[0].charAt(0).toUpperCase(),
+                    middleInitial = authorNameArray[1].charAt(0).toUpperCase(),
+                    authorFullRef = '' 
+                switch (this.style) {
+                    case "APA":
+                        authorFullRef = `${lastName}, ${firstInitial}. ${middleInitial}`;
+                        manyAuthors.push(authorFullRef);
+                    case "Harvard":
+                        authorFullRef = `${lastName}, ${firstInitial}${middleInitial}`;
+                        manyAuthors.push(authorFullRef);
+                }
+            })
+            let finalList = '', finalName = manyAuthors[manyAuthors.length -1]
+    
+            for (let i = 0;  i < manyAuthors.length - 1; i ++) {
+                finalList +=  manyAuthors[i] + ', '
+            }
+            finalList += ` & ${finalName}`
+            return finalList
+        }
+    }
+}
+
